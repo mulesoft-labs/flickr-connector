8151febedfbbac28369c543d55c66c085a81316c
Emiliano Lesende <emiliano.lesende@gmail.com>
2011-03-15 15:13:16 -0300

    Updated README

--- src/main/java/org/mule/ibeans/flickr/FlickrSearchIBean.java
+++ src/main/java/org/mule/ibeans/flickr/FlickrSearchIBean.java
@@ -131,96 +131,20 @@ public interface FlickrSearchIBean extends FlickrBase
      * @param maxTakenDate    Maximum taken date. Photos with an taken date less than or equal to this value will be returned. The date should be in the form of a mysql datetime.
      * @param license         The license id for photos (for possible values see the flickr.photos.licenses.getInfo method). Multiple licenses may be comma-separated.
      * @param sort            The order in which to sort returned photos. Deafults to date-posted-desc (unless you are doing a radial geo query, in which case the default sorting is by ascending distance from the point specified). The possible values are: date-posted-asc, date-posted-desc, date-taken-asc, date-taken-desc, interestingness-desc, interestingness-asc, and relevance.
-     * @param privacyFilter   Return photos only matching a certain privacy level. This only applies when making an authenticated call to view photos you own. Valid values are:
-     *                        <p/>
-     *                        1 public photos
-     *                        2 private photos visible to friends
-     *                        3 private photos visible to family
-     *                        4 private photos visible to friends & family
-     *                        5 completely private photos
-     *                        <p/>
-     * @param bbox            A comma-delimited list of 4 values defining the Bounding Box of the area that will be searched.
-     *                        <p/>
-     *                        The 4 values represent the bottom-left corner of the box and the top-right corner, minimum_longitude, minimum_latitude, maximum_longitude, maximum_latitude.
-     *                        <p/>
-     *                        Longitude has a range of -180 to 180 , latitude of -90 to 90. Defaults to -180, -90, 180, 90 if not specified.
-     *                        <p/>
-     *                        Unlike standard photo queries, geo (or bounding box) queries will only return 250 results per page.
-     *                        <p/>
-     *                        Geo queries require some sort of limiting agent in order to prevent the database from crying. This is basically like the check against "parameterless searches" for queries without a geo component.
-     *                        <p/>
-     *                        A tag, for instance, is considered a limiting agent as are user defined min_date_taken and min_date_upload parameters � If no limiting factor is passed we return only photos added in the last 12 hours (though we may extend the limit in the future).
-     * @param accuracy        (Optional)
-     *                        Recorded accuracy level of the location information. Current range is 1-16 :
-     *                        <p/>
-     *                        World level is 1
-     *                        Country is ~3
-     *                        Region is ~6
-     *                        City is ~11
-     *                        Street is ~16
-     *                        <p/>
-     *                        Defaults to maximum value if not specified.
-     * @param safeSearch      (Optional)
-     *                        Safe search setting:
-     *                        <p/>
-     *                        1 for safe.
-     *                        2 for moderate.
-     *                        3 for restricted.
-     *                        <p/>
-     *                        (Please note: Un-authed calls can only see Safe content.)
-     * @param contentType     Content Type setting:
-     *                        <p/>
-     *                        1 for photos only.
-     *                        2 for screenshots only.
-     *                        3 for 'other' only.
-     *                        4 for photos and screenshots.
-     *                        5 for screenshots and 'other'.
-     *                        6 for photos and 'other'.
-     *                        7 for photos, screenshots, and 'other' (all).
-     *                        <p/>
-     * @param machineTags     Aside from passing in a fully formed machine tag, there is a special syntax for searching on specific properties :
-     *                        <p/>
-     *                        Find photos using the 'dc' namespace : "machine_tags" => "dc:"
-     *                        Find photos with a title in the 'dc' namespace : "machine_tags" => "dc:title="
-     *                        Find photos titled "mr. camera" in the 'dc' namespace : "machine_tags" => "dc:title=\"mr. camera\"
-     *                        Find photos whose value is "mr. camera" : "machine_tags" => "*:*=\"mr. camera\""
-     *                        Find photos that have a title, in any namespace : "machine_tags" => "*:title="
-     *                        Find photos that have a title, in any namespace, whose value is "mr. camera" : "machine_tags" => "*:title=\"mr. camera\""
-     *                        Find photos, in the 'dc' namespace whose value is "mr. camera" : "machine_tags" => "dc:*=\"mr. camera\""
-     *                        <p/>
-     *                        Multiple machine tags may be queried by passing a comma-separated list. The number of machine tags you can pass in a single query depends on the tag mode (AND or OR) that you are querying with. "AND" queries are limited to (16) machine tags. "OR" queries are limited to (8).
-     * @param machineTagsMode (Required) - Either 'any' for an OR combination of tags, or 'all' for an AND combination. Defaults to 'any' if not specified.
+     * @param privacyFilter   Return photos only matching a certain privacy level. This only applies when making an authenticated call to view photos you own. Valid values are: 1 public photos. 2 private photos visible to friends. 3 private photos visible to family. 4 private photos visible to friends & family. 5 completely private photos
+     * @param bbox            A comma-delimited list of 4 values defining the Bounding Box of the area that will be searched. The 4 values represent the bottom-left corner of the box and the top-right corner, minimum_longitude, minimum_latitude, maximum_longitude, maximum_latitude.
+     * @param accuracy        Recorded accuracy level of the location information. Current range is 1-16 : World level is 1. Country is ~3. Region is ~6. City is ~11. Street is ~16. Defaults to maximum value if not specified.
+     * @param safeSearch      Safe search setting: 1 for safe. 2 for moderate. 3 for restricted. (Please note: Un-authed calls can only see Safe content.)
+     * @param contentType     Content Type setting: 1 for photos only. 2 for screenshots only. 3 for 'other' only. 4 for photos and screenshots. 5 for screenshots and 'other'. 6 for photos and 'other'. 7 for photos, screenshots, and 'other' (all).
+     * @param machineTags     Aside from passing in a fully formed machine tag, there is a special syntax for searching on specific properties. Multiple machine tags may be queried by passing a comma-separated list. The number of machine tags you can pass in a single query depends on the tag mode (AND or OR) that you are querying with. "AND" queries are limited to (16) machine tags. "OR" queries are limited to (8).
+     * @param machineTagsMode Either 'any' for an OR combination of tags, or 'all' for an AND combination. Defaults to 'any' if not specified.
      * @param groupId         The id of a group who's pool to search. If specified, only matching photos posted to the group's pool will be returned.
      * @param contacts        Search your contacts. Either 'all' or 'ff' for just friends and family. (Experimental)
      * @param woeId           A 32-bit identifier that uniquely represents spatial entities. (not used if bbox argument is present).
-     *                        <p/>
-     *                        Geo queries require some sort of limiting agent in order to prevent the database from crying. This is basically like the check against "parameterless searches" for queries without a geo component.
-     *                        <p/>
-     *                        A tag, for instance, is considered a limiting agent as are user defined min_date_taken and min_date_upload parameters &emdash; If no limiting factor is passed we return only photos added in the last 12 hours (though we may extend the limit in the future).
      * @param placeId         A Flickr place id. (not used if bbox argument is present).
-     *                        <p/>
-     *                        Geo queries require some sort of limiting agent in order to prevent the database from crying. This is basically like the check against "parameterless searches" for queries without a geo component.
-     *                        <p/>
-     *                        A tag, for instance, is considered a limiting agent as are user defined min_date_taken and min_date_upload parameters &emdash; If no limiting factor is passed we return only photos added in the last 12 hours (though we may extend the limit in the future).
      * @param media           Filter results by media type. Possible values are all (default), photos or videos
-     * @param hasGeo          (Optional)
-     *                        Any photo that has been geotagged, or if the value is "0" any photo that has not been geotagged.
-     *                        <p/>
-     *                        Geo queries require some sort of limiting agent in order to prevent the database from crying. This is basically like the check against "parameterless searches" for queries without a geo component.
-     *                        <p/>
-     *                        A tag, for instance, is considered a limiting agent as are user defined min_date_taken and min_date_upload parameters &emdash; If no limiting factor is passed we return only photos added in the last 12 hours (though we may extend the limit in the future).
-     * @param geoContext      Geo context is a numeric value representing the photo's geotagginess beyond latitude and longitude. For example, you may wish to search for photos that were taken "indoors" or "outdoors".
-     *                        <p/>
-     *                        The current list of context IDs is :
-     *                        <p/>
-     *                        0, not defined.
-     *                        1, indoors.
-     *                        2, outdoors.
-     *                        <p/>
-     *                        <p/>
-     *                        Geo queries require some sort of limiting agent in order to prevent the database from crying. This is basically like the check against "parameterless searches" for queries without a geo component.
-     *                        <p/>
-     *                        A tag, for instance, is considered a limiting agent as are user defined min_date_taken and min_date_upload parameters &emdash; If no limiting factor is passed we return only photos added in the last 12 hours (though we may extend the limit in the future).
+     * @param hasGeo          Any photo that has been geotagged, or if the value is "0" any photo that has not been geotagged.
+     * @param geoContext      Geo context is a numeric value representing the photo's geotagginess beyond latitude and longitude. For example, you may wish to search for photos that were taken "indoors" or "outdoors". The current list of context IDs is : 0, not defined. 1, indoors. 2, outdoors.
      * @param lat             A valid latitude, in decimal format, for doing radial geo queries.
      * @param lon             A valid longitude, in decimal format, for doing radial geo queries.
      * @param radius          A valid radius used for geo queries, greater than zero and less than 20 miles (or 32 kilometers), for use with point-based geo queries. The default value is 5 (km).
